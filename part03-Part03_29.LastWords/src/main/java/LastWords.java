8daa94418d638201ffc38951049982e8aadde308
gurbanovnihad <7404@holbertonstudents.com>
2024-09-15 12:00:06 +0400

    last words

---
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class LastWords {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        while(true) {
+            String input = scanner.nextLine();
+            
+            if(input.isEmpty()) {
+                break;
+            } else {
+                String[] data = input.split(" ");
+                System.out.println(data[data.length - 1]);
+            }
+        }
+
+
+    }
+}
